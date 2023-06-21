724ddbc39e05b316e736d35325d8dd7b8a65723e
Steeve <steevemx98@gmail.com>
2023-06-21 09:21:46 +0100

    err

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,14 @@
+/*=============== LOADER ===============*/
+
+var loader = document.getElementById("loader");
+window.addEventListener("load", function(){
+    loader.style.display = 'none';
+})
+
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
@@ -34,35 +41,35 @@ const blurHeader = () =>{
     const header = document.getElementById('header')
     // When the scroll is greater than 50 viewport height, add the blur-header class to the header tag
     this.scrollY >= 50  ? header.classList.add('blur-header') 
-                       : header.classList.remove('blur-header')
+    : header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 const scrollUp = () =>{
-	const scrollUp = document.getElementById('scroll-up')
+    const scrollUp = document.getElementById('scroll-up')
     // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
 	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-						: scrollUp.classList.remove('show-scroll')
+    : scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
-    
+
 const scrollActive = () =>{
-  	const scrollY = window.pageYOffset
-
+    const scrollY = window.pageYOffset
+    
 	sections.forEach(current =>{
-		const sectionHeight = current.offsetHeight,
-			  sectionTop = current.offsetTop - 58,
-			  sectionId = current.getAttribute('id'),
-			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
-
+        const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 58,
+        sectionId = current.getAttribute('id'),
+        sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+        
 		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-			sectionsClass.classList.add('active-link')
+            sectionsClass.classList.add('active-link')
 		}else{
-			sectionsClass.classList.remove('active-link')
+            sectionsClass.classList.remove('active-link')
 		}                                                    
 	})
 }
